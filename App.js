2307ed2c865a1c649f2b46b4ef985b21978b99ae
ken0426 <kenkaitixyou0426@gmail.com>
2022-02-27 11:39:48 +0900

    雛形となるコンポーネントの作成

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}></View>
+        <View style={styles.rightContainer}></View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    backgroundColor: 'red',
+    width: 100,
+  },
+  rightContainer: {
+    backgroundColor: 'blue',
+    flex: 1,
+  }
 });
